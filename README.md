fb5a1610f6ca2b8bbfc9ff2b2daaa23c2685841b
Gonçalo Morais <gnclmorais@gmail.com>
2015-12-30 17:53:06 +0000

    Use Promises to create lists.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Creates a Trello board for a certain year, including a list for each week.
 
 
 ## Instructions
+
+## Resources
+* [Trello’s API rate limits](http://help.trello.com/article/838-api-rate-limits)
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var http = require('http')
 var url = require('url')
 var OAuth = require('oauth').OAuth
 var open = require('open')
+var Promise = require("bluebird")
+var moment = require('moment')
 
 var trelloKey = process.env.TRELLO_KEY
 var trelloSecret = process.env.TRELLO_SECRET
@@ -56,13 +58,19 @@ function callback(req, res) {
       var boardId = board.id
       console.log('boardId:', boardId, typeof boardId)
 
-      oa.post("https://api.trello.com/1/lists", accessToken, accessTokenSecret, {
-        name: 'Week 1',
-        idBoard: boardId,
-        pos: 'bottom',
-      }, null, function (error, list, response) {
-        console.log('list:', list)
+
+      createLists(boardId, dateToWeekNames('2016'), accessToken, accessTokenSecret)
+/*
+      dateToWeekNames('2016').forEach(function (name) {
+        oa.post("https://api.trello.com/1/lists", accessToken, accessTokenSecret, {
+          name: name,
+          idBoard: boardId,
+          pos: 'bottom',
+        }, null, function (error, list, response) {
+          console.log('list:', list)
+        })
       })
+*/
 
       serveClosingPage(res)
     })
@@ -76,6 +84,58 @@ function serveClosingPage(res) {
   })
   res.write('<pre>You may close this tab now :)</pre>')
   res.end()
+}
+
+function dateToWeekNames(date) {
+  var result = []
+  var start = new Date(date.toString())
+  var nextYear = start.getFullYear() + 1
+
+  // Start on a Monday
+  while (start.getDay() !== 1) {
+    start.setDate(start.getDate() - 1)
+  }
+
+  var a
+  while (start.getFullYear() < nextYear) {
+    a = new Date(start.toDateString())
+    start.setDate(a.getDate() + 6)
+    result.push(
+      'Week ' + moment(a).week() + ' / ' +
+      moment(a.toISOString()).format('MMM. Do') + '-' +
+      moment(start.toISOString()).format('MMM. Do')
+    )
+    start.setDate(start.getDate() + 1)
+  }
+
+  return result.filter(function (value, index) {
+    return index < 10
+  })
+}
+
+function createLists(boardId, names, accessToken, accessTokenSecret) {
+  return Promise.mapSeries(names, function (name) {
+    return createList(boardId, name, accessToken, accessTokenSecret)
+  })
+}
+
+function createList(boardId, name, accessToken, accessTokenSecret) {
+  return new Promise(function (resolve, reject) {
+    oa.post('https://api.trello.com/1/lists', accessToken, accessTokenSecret, {
+      name: name,
+      idBoard: boardId,
+      pos: 'bottom',
+    }, null, function (error, list, response) {
+      if (error) {
+        console.log('error:', error)
+        reject(error)
+        return
+      }
+
+      console.log('list:', list)
+      resolve()
+    })
+  })
 }
 
 
